65804d7bdf1096f5500ac7c85c9726ce900b7545
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-11 18:37:28 -0300

    Resolvendo exercicio desafio barOOJava_DevSuperior

---
+++ src/entities/Bill.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Bill {
+	// Atributos
+	// Conta do bar em inglês = Bill
+	public char gender;    // gender = sexo
+	public int beer;       // beer = cerveja
+	public int barbecue;   // barbecue = espetinhos
+	public int softDrink;  // softDrink = refrigerante
+	
+	// Métodos
+	public double cover() {  // Cover = Cover artitistico
+		return 
+	}
+	
+	public double feeding() {  // Feeding = consumo
+		return 
+	}
+	
+	
+}
