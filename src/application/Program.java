fce78e9ae45097e2ae958fe6f31d8794fc6138b9
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-11 19:10:08 -0300

    Resolvendo o desafio BarOO_Java - DevSuperior

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,34 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Bill;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Desafio Bar_OO_Java_DevSuperior
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Bill bill = new Bill();
+		
+		System.out.print("Sexo: ");
+		bill.gender = sc.next().charAt(0);
+		System.out.print("Quantidade de cervejas: ");
+		bill.beer = sc.nextInt();
+		System.out.print("Quantidade de refrigerantes: ");
+		bill.softDrink = sc.nextInt();
+		System.out.print("Quantidade de espetinhos: ");
+		bill.barbecue = sc.nextInt();
+		
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", bill.feeding());
+		
+		
+		sc.close();
 
 	}
 
--- src/entities/Bill.java
+++ src/entities/Bill.java
@@ -10,12 +10,26 @@ public class Bill {
 	
 	// Métodos
 	public double cover() {  // Cover = Cover artitistico
+		couvert = 0.00;
+		if(feeding() > 30) {
+			couvert = 0.00;
+			System.out.println("Isento de Couvert");
+		}else {
+			couvert = 4.00;
+		}
 		return 
 	}
 	
-	public double feeding() {  // Feeding = consumo
+	public double feeding() {  // Feeding = Consumo
+		return ( (beer * 5.00) + (barbecue * 7.00) + (softDrink * 3.00));
+	}
+	
+	public double ticket() {   // Ticket = Ingresso
 		return 
 	}
 	
+	public double total() {    // Total = Conta total
+		return  
+	}
 	
 }
